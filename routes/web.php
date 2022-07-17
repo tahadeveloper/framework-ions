cde6ccc1ade7573727032baae42eeeebe3016d11
ahmedhamdi <taha.developer@outlook.com>
2022-07-17 10:09:11 +0200

    update libs

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,22 @@
 <?php
 
-use Ions\Bundles\MRoute;
+use Ions\Bundles\MRoute as Route;
 use Ions\Bundles\Path;
 
-MRoute::get('/index', static function (){
-   echo 'Hello world';
+Route::get('/index', static function () {
+    echo 'Hello world';
 })->save();
 
-MRoute::match(['get','post'],'/', 'Home::index')->save();
+Route::match(['get', 'post'], '/', 'Home::index');
+Route::match(['get', 'post'], '/hello/{name}', 'Home::index', ['id' => 'taha'])
+    ->name('test')
+    ->where(['name' => '\d+'])
+    ->save();
+
+Route::prefix('/home')->group(static function () {
+    Route::prefix('/index')->group(static function () {
+        Route::get('/test', 'Home::index')->save();
+    });
+});
 
 include_once Path::route('web/super.php');
