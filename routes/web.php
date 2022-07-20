0cab88756c9e110f9c6bf3459b6157a9b9bbf403
ahmedhamdi <taha.developer@outlook.com>
2022-07-20 15:07:30 +0200

    add options in route - render twig

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Ions\Bundles\Route;
 use Ions\Bundles\Path;
+use Ions\Support\Request;
 
 Route::get('/', 'Home::index');
-Route::match(['get', 'post'], '/index', static function () {
+Route::match(['get', 'post'], '/index', static function (Request $request) {
+    appSetLocale('ar');
+    render('re.html.twig', ['name' => 'ionzile', 'text' => $request->get('text')]);
     echo 'Hello world';
 });
 
